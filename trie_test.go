d2922edfa9ae8c46c35eec61fe4bdc797ea14f75
sun-moon-star-star <75414051+sun-moon-star-star@users.noreply.github.com>
2021-02-25 14:43:13 +0800

    test(trie_test): BenchmarkTrie

--- trie_test.go
+++ trie_test.go
@@ -14,6 +14,7 @@ func TestTrie(t *testing.T) {
 	trie.Write("height", 155.5)
 	trie.Write("/height", 155.8)
 	trie.Write("age", 22)
+	trie.Write("agename", 22)
 	trie.Write("///", "hello world")
 
 	bytes, err := json.Marshal(trie)
@@ -31,4 +32,30 @@ func TestTrie(t *testing.T) {
 		t.Fatal(err)
 	}
 	t.Log(string(bytes))
+}
+
+func BenchmarkTrie(b *testing.B) {
+	trie := trie.New()
+	trie.Write("/name", "zhaolu")
+	trie.Write("/name/alias", "rongminglu")
+	trie.Write("height", 155.5)
+	trie.Write("/height", 155.8)
+	trie.Write("age", 22)
+	trie.Write("agename", 22)
+
+	bytes, err := json.Marshal(trie)
+	if err != nil {
+		b.Fatal(err)
+	}
+	b.Log(string(bytes))
+
+	node := trie.Read("/height")
+	if node == nil {
+		b.Fatal(errors.New("which node should be exists but not"))
+	}
+	bytes, err = json.Marshal(node)
+	if err != nil {
+		b.Fatal(err)
+	}
+	b.Log(string(bytes))
 }
